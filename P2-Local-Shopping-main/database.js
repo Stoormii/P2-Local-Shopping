5efe44f3334dcc288fc34d468fd44747fd3dd208
Magnus170 <ju03rb@student.aau.dk>
2025-04-08 10:31:32 +0200

    Magnus

--- P2-Local-Shopping-main/database.js
+++ P2-Local-Shopping-main/database.js
@@ -15,7 +15,7 @@ const pool = mysql.createPool({
 // Funktion til at hente alle brugere ud fra databasen
 export async function getUsers() { 
     const[rows] = await pool.query("SELECT * FROM users") // SQL-Query for at hente alle brugere
-    return rows                                           // Returnerer bugerne
+    return rows                                           // Returnerer brugere
 }
 
 // Funktion til at indsætte en ny bruger i databasen
@@ -25,6 +25,23 @@ export async function createUser(firstname, email, password){
     VALUES(?,?,?)`,
      [firstname, email, password]) // Indsætter værdierne sikkert i databasen
     return result
+}
+
+// Funktion til at indsætte items i databasen (Skal ændres, når jeg har funktionen til at hente items)
+// Skal ændres, så den kan hente fra en fil, når serveren er oppe og køre
+export async function createItem(Product_name, Category_ID, Store_ID, Quantity, Description, Price, image) {
+    const result = await pool.query(`
+        INSERT INTO Products(Product_name, Category_ID, Store_ID, Quantity, Description, Price, image)
+        VALUES(?,?,?,?,?,?,?)`,
+    )
+    return result
+}
+
+//Funktion til at lave en ordre (Skal ændres, når funktionen til at foretage køb er lavet)
+export async function createOrder(Order_Date, Delivery_ID, Customer_ID, Order_Total) {
+    const result = await pool.query(`
+        INSERT INTO Orders(Order_Date, Delivery_ID, Customer_ID, Order_Total)
+        VALUES(?,?,?,?)`,)
 }
 
 export {pool} // Eksporterer poolen til brug i andre filer
