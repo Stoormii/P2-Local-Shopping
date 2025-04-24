8545a0a9146e2cadfcf7f09b47d245a9cb0c4668
Magnus170 <ju03rb@student.aau.dk>
2025-04-24 21:28:06 +0200

    1

--- P2-Local-Shopping-main/app.js
+++ P2-Local-Shopping-main/app.js
@@ -267,13 +267,14 @@ const upload = multer({ storage });
 app.post('/upload-image', upload.single('image'), (req, res) => {
     try {
         if (!req.file) {
+            console.error('No file uploaded');
             return res.status(400).json({ message: 'No file uploaded' });
         }
 
-        // Brug en miljøvariabel til base-URL
-        const baseUrl = `${req.protocol}://${req.get('host')}`;
-        const imageUrl = `/uploads/${req.file.filename}`;
-            res.status(200).json({ imageUrl });
+        console.log('Uploaded file:', req.file); // Log filoplysninger
+
+        const protocol = process.env.NODE_ENV === 'production' ? 'https' : req.protocol;
+        const imageUrl = `${protocol}://${req.get('host')}/uploads/${req.file.filename}`;
         console.log('Generated image URL:', imageUrl);
 
         res.status(200).json({ imageUrl });
