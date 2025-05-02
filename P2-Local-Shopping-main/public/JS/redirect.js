c0f7fc0ac8a3071740cc0e055fe84971a5814ab3
Lassewest <lasse.viborg@gmail.com>
2025-05-02 15:07:25 +0200

    Create redirect.js
    Mindre funktion til at sende id videre

---
+++ P2-Local-Shopping-main/public/JS/redirect.js
@@ -0,0 +1,10 @@
+//This function is used to redirect the user to the store page when they click on an image
+function redirect2(imageId){
+   window.location.href = `store/${imageId}`;
+   }
+
+
+   //This function is used to redirect the user to the item page when they click on an image
+function redirect1(imageId){
+   window.location.href = `item/${imageId}`;
+   }
