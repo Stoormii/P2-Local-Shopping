44b291705843ad425faa097b1aabdb041c7c8303
Lassewest <lwest24@student.aau.dk>
2025-05-26 22:14:17 +0200

    Update app.js

--- P2-Local-Shopping-main/app.js
+++ P2-Local-Shopping-main/app.js
@@ -536,6 +536,7 @@ app.get('/store/:id', async (req, res) => {
     <meta charset="UTF-8">
     <title>${Store.Store_name}</title>
     <link rel="stylesheet" href="/css/store-template.css">
+    <link rel="stylesheet" href="/css/ValentinoStylesheet.css">
     <style>
         #map {
             height: 400px;
