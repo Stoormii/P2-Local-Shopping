3004b3fc6ca3b900bf6123caccde8e828a55c797
Stoormii <osoren24@student.aau.dk>
2025-05-27 14:10:55 +0200

    Update Store-Admin.js

--- P2-Local-Shopping-main/public/JS/Store-Admin.js
+++ P2-Local-Shopping-main/public/JS/Store-Admin.js
@@ -139,7 +139,11 @@ document.addEventListener('DOMContentLoaded', function () {
         categorySelect.value                                = p.Category_ID;
         document.getElementById('productStock').value       = p.Quantity;
         document.getElementById('productDescription').value = p.Description;
-        document.getElementById('productPrice').value       = p.Price;
+        document.getElementById('productImage').dataset.imageUrl = p.image || '';
+
+        document.getElementById('imagePreview').innerHTML = p.image
+        ? `<img src="${p.image}" alt="Preview" />`
+        : '';
         openModal(true);
     }
 
